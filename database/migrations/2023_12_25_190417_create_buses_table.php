eff991c5b00629975694e11b0c10f5ecc86d3d75
creativehimel <himel.phy@bsmrstu.edu.bd>
2023-12-26 01:10:05 +0600

    all migration table added

--- database/migrations/2023_12_25_190417_create_buses_table.php
+++ database/migrations/2023_12_25_190417_create_buses_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('buses', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('bus_name');
+            $table->foreignId('location_id')->constrained('locations');
+            $table->integer('total_seats')->default(36);
+            $table->string('bus_image')->nullable();
+            $table->boolean('status')->default(1)->comment('0 = inactive; 1 = active');
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('buses');
     }
 };
